aa9fac93ce2170d8f1a1a2925482d51b2efb94a0
Shravaniuppar <119950387+Shravaniuppar@users.noreply.github.com>
2024-08-03 17:59:41 +0530

    3b.py

--- 3b.py
+++ 3b.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug  3 16:40:45 2024
-
-@author: Administrator
-"""
-
+import math
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+if a%2!=0 and b%2!=0 and a!=b:
+    a=a+b
+    b=a-b
+    a=a-b
+    print('a=',a)
+    print('b=',b)
+elif a%2==0 and b%2==0 and a!=b:
+    if a>b:
+        print(f"{a} is largest")
+    else:
+        print(f"{b} is largest")
+else:
+    a=math.factorial(a)
+    b=math.factorial(b)
+    print("Factorial")
